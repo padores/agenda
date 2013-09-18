9195742d5b5540439f576edc5dc15dbe49f3aeb0
Pedrosa <pedrosa@ubuntu.ubuntu-domain>
2013-09-18 18:44:04 -0300

    Adicionando construtores

--- Contato.java
+++ Contato.java
@@ -4,7 +4,16 @@ public class Contato {
 	private String telefone;
 	
 	public Contato(){
-		
+		this.nome = "-";
+	}
+	
+	public Contato(String umNome){
+		this.nome=umNome;
+	}
+	
+	public Contato(String umNome, String umTelefone){
+		this.nome=umNome;
+		this.telefone=umTelefone;
 	}
 	
 }
